814263cf1fd7cb93fd99b805b3d18b50a946efc2
WRLNH <wuyutong030111@gmail.com>
2024-10-17 23:20:21 +0800

    实现了部分Gimmickalize.Barlines

---
+++ Gimmickalizer/Gimmickalize.cs
@@ -0,0 +1,183 @@
+﻿using NLog;
+using NLog.Targets.Helper;
+using NLog.Targets.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gimmickalizer.Views;
+
+namespace Gimmickalizer
+{
+    public class TimingPonits
+    {
+        /// <summary>
+        /// Offset为offset
+        /// BPM为BPM/SV速度参数
+        /// TimeSignature为节拍
+        /// Sample为音效种类  1为Normal 2为Soft 3为Drum
+        /// SampleNumber为音效号码
+        /// Volume为音量
+        /// RedOrGreen为 红线 或 绿线  1为红线 0为绿线
+        /// KiaiMode为Kiai Mode
+        /// </summary>
+        public int Offset;
+        public string BPM;
+        public int TimeSignature;
+        public int Sample;
+        public int SampleNumber;
+        public short Volume;
+        public short RedOrGreen;
+        public short KiaiMode;
+    }
+
+    public class HitObjects
+    {
+        /// <summary>
+        /// XCoordinate为X坐标
+        /// YCoordinate为Y坐标
+        /// Offset为时间点
+        /// Type为类型  1为Circle 2为Slider 12为Spinner
+        /// DonOrKat为Circle类型 0为o 2或8为x 4为O 6或12为X
+        /// SliderSomthing为Slider的一些信息  Taiko里似乎没啥用 仅Slider该成员有值
+        /// SliderRepeatTimes为Slider的一些信息  Taiko里似乎没啥用 仅Slider该成员有值
+        /// OffsetLength为滑条长度  仅Slider该成员有值
+        /// SpinnerSample不确定  一般为0 仅Spinner该成员有值
+        /// SpinnerEndOffset为转盘结束时间点  仅Spinner该成员有值
+        /// UselessEnding不知道是啥  一般为"0:0:0:0:" 仅Circle和Spinner该成员有值
+        /// </summary>
+        public int XCoordinate;
+        public int YCoordinate;
+        public int Offset;
+        public short Type;
+        public short DonOrKat;
+        public string SliderSomething;
+        public int SliderRepeatTimes;
+        public int OffsetLength;
+        public int SpinnerSample;
+        public int SpinnerEndOffset;
+        public string UselessEnding;
+    }
+
+    public class Gimmickalize
+    {
+        public static string GetDiffName(string Name)
+        {
+            string temp = "";
+            string diffName = "";
+
+            int i;
+            for (i = Name.Length - 1; i >= 0; i--)
+            {
+                if (Name.ToString()[i] == ']')
+                {
+                    i--;
+                    break;
+                }
+            }
+            for (; Name.ToString()[i] != '['; i--)
+            {
+                temp += Name[i];
+            }
+            for (i = temp.Length - 1; i >= 0; i--)
+            {
+                diffName += temp[i];
+            }
+
+            return diffName;
+        }
+
+        public static void Barlines(StreamReader sr, string diffName, Logger logger)
+        {
+            string line;
+            string flag = "";
+            List<string> readLines = [];
+            List<string> outLines = [];
+            List<string> strLstTimingPoints = [];
+            List<string> strLstHitObjects = [];
+            List<TimingPonits> timingPonits = [];
+            List<HitObjects> hitObjects = [];
+
+            while((line = sr.ReadLine()) != null)
+            {
+                if (line == "Mode: 0" || line == "Mode: 2" || line == "Mode: 3")
+                {
+                    logger.Error("模式错误, 请检查所选难度的模式...");
+                    return;
+                }
+
+                if (line.Length >= 8)
+                {
+                    if (line.Substring(0, 8) == "Version:")
+                    {
+                        readLines.Add(line);
+                        outLines.Add(line + "Barlines");
+                        continue;
+                    }
+                }
+
+                if (line == "[TimingPoints]")
+                {
+                    readLines.Add(line);
+                    outLines.Add(line);
+                    flag = "TimingPoints";
+                    continue;
+                }
+                else if (line == "[HitObjects]")
+                {
+                    readLines.Add(line);
+                    outLines.Add(line);
+                    flag = "HitObjects";
+                    continue;
+                }
+
+                if (flag == "TimingPoints" && line != "")
+                {
+                    readLines.Add(line);
+                    outLines.Add(line);
+                    strLstTimingPoints.Add(line);
+                    continue;
+                }
+                else if (flag == "HitObjects" && line != "")
+                {
+                    readLines.Add(line);
+                    outLines.Add(line);
+                    strLstHitObjects.Add(line);
+                    continue;
+                }
+
+                readLines.Add(line);
+                outLines.Add(line);
+            }
+
+            for (int i = 0; i < strLstTimingPoints.Count; i++)
+            {
+                string[] subs = strLstTimingPoints[i].Split(',');
+                TimingPonits temp = new TimingPonits();
+                temp.Offset = Convert.ToInt32(subs[0]);
+                temp.BPM = subs[1];
+                temp.TimeSignature = Convert.ToInt32(subs[2]);
+                temp.Sample = Convert.ToInt32(subs[3]);
+                temp.SampleNumber = Convert.ToInt32(subs[4]);
+                temp.Volume = Convert.ToInt16(subs[5]);
+                temp.RedOrGreen = Convert.ToInt16(subs[6]);
+                temp.KiaiMode = Convert.ToInt16(subs[7]);
+                timingPonits.Add(temp);
+            }
+
+            for (int i = 0; i < timingPonits.Count; i++)
+            {
+                Debug.WriteLine(timingPonits[i].Offset.ToString() + ',' + timingPonits[i].BPM + ',' + timingPonits[i].TimeSignature.ToString() + ',' + timingPonits[i].Sample.ToString() + ',' + timingPonits[i].SampleNumber.ToString() + ',' + timingPonits[i].Volume.ToString() + ',' + timingPonits[i].RedOrGreen.ToString() + ',' + timingPonits[i].KiaiMode.ToString());
+            }
+            logger.Info("Done!");
+        }
+
+        public static void YellowNotes(StreamWriter sr, string diffName)
+        {
+
+        }
+    }
+}
--- Gimmickalizer/Views/RootView.xaml.cs
+++ Gimmickalizer/Views/RootView.xaml.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using Gimmickalizer;
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
 
 namespace Gimmickalizer.Views
 {
     public partial class RootView : HandyControl.Controls.Window
     {
-        private Logger logger = LogManager.GetCurrentClassLogger();
+        public Logger logger = LogManager.GetCurrentClassLogger();
+        public List<FileInfo> lstFiles;
+        public string inputFilePath;
 
         public RootView()
         {
@@ -21,7 +26,7 @@ namespace Gimmickalizer.Views
                 var target = new WpfRichTextBoxTarget
                 {
                     Name = "RichText",
-                    Layout = "[${longdate:useUTC=false}] :: [${level:uppercase=true}] :: ${logger}:${callsite} :: ${message} ${exception:innerFormat=tostring:maxInnerExceptionLevel=10:seperator=,:format=tostring}",
+                    Layout = "[${time}] [${level:uppercase=true}] :: ${message} ${exception:innerFormat=tostring:maxInnerExceptionLevel=10:seperator=,:format=tostring}",
                     ControlName = "LogRichTextBox",
                     FormName = GetType().Name,
                     AutoScroll = true,
@@ -34,7 +39,6 @@ namespace Gimmickalizer.Views
                 LogManager.Configuration.LoggingRules.Insert(0, new NLog.Config.LoggingRule("*", LogLevel.Info, asyncWrapper));
                 LogManager.ReconfigExistingLoggers();
             });
-            
         }
 
         private void ReadPath_Click(object sender, RoutedEventArgs e)
@@ -43,8 +47,7 @@ namespace Gimmickalizer.Views
             {
                 // 获取指定的.osu文件夹路径
                 DirectoryInfo FolderInfo = new DirectoryInfo(FolderPath.Text);
-                FileInfo[] Files = FolderInfo.GetFiles();
-                List<FileInfo> lstFiles = Files.ToList();
+                lstFiles = FolderInfo.GetFiles().ToList();
                 for (int i = 0; i < lstFiles.Count; i++)
                 {
                     if (lstFiles[i].Extension == ".osu")
@@ -58,18 +61,46 @@ namespace Gimmickalizer.Views
             {
                 if (FolderPath.Text == "" || FolderPath.Text == null)
                 {
-                    logger.Info("你不输入路径想干嘛!");
+                    logger.Error("你不输入路径想干嘛!");
                 }
                 else
                 {
-                    logger.Info("路径错啦! 检查下吧");
+                    logger.Error("路径错啦! 检查下吧");
                 }
             }
         }
 
         private void Gimmickalize_Click(object sender, RoutedEventArgs e)
         {
-            logger.Info("111");
+            logger.Info("开始Gimmickalize...");
+            try
+            {
+                try
+                {
+                    string diffName = Gimmickalize.GetDiffName(DifficultySelect.SelectedItem.ToString());
+                    StreamReader openFile = new StreamReader(FolderPath.Text + '\\' + DifficultySelect.SelectedItem.ToString());
+                    logger.Info("File opened");
+
+                    if (GimmickTypeSelect.SelectedItem.ToString() == "System.Windows.Controls.ComboBoxItem: Barlines")
+                    {
+                        Gimmickalize.Barlines(openFile, diffName, logger);
+                    }
+                    else if (GimmickTypeSelect.SelectedItem.ToString() == "System.Windows.Controls.ComboBoxItem: Yellow Notes")
+                    {
+
+                    }
+
+                    openFile.Close();
+                }
+                catch
+                {
+                    logger.Error("Gimmickalize失败...");
+                }
+            }
+            catch
+            {
+                logger.Error("打开文件失败...");
+            }
         }
     }
 }
